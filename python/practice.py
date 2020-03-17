65b6672a760083853d1c6bc01607942d2f7e7a87
h590fadzai <61730803+h590fadzai@users.noreply.github.com>
2020-03-17 16:51:51 +0800

    Bugfixing
    fixed the keys on line 23 to "/loaction/ and changed the letter Y to a capital letter in line 8.

--- python/practice.py
+++ python/practice.py
@@ -5,7 +5,7 @@ import datetime
 # Init Variables
 welcomeMessage = "--- Weather Pyvien ---"
 date = datetime.datetime.now()
-date = date.strftime("%y/%-m/%-d")
+date = date.strftime("%Y/%-m/%-d")
 
 # Welcome to the Weather API
 print(welcomeMessage)
@@ -20,9 +20,9 @@ data = json.loads(response.read())
 woeid = data[0]['woeid']
 
 # Get Weather
-link = 'https://www.metaweather.com/api/???/' + int(woeid) + '/' + date
+link = 'https://www.metaweather.com/api/location/' + str(woeid) + '/' + date
 response = urlopen(link)
-data = json.loads(response.read()
+data = json.loads(response.read())
 
 # Print it to the screen
 print("Type of weather: " + data[0]["weather_state_name"])
