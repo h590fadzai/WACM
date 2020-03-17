19101e69f28f1396fcf3fc954e418884a17e83b7
Adam <33823436+DerpyDinosaur@users.noreply.github.com>
2020-03-17 12:26:27 +0800

    Buggy Api

--- python/practice.py
+++ python/practice.py
@@ -1,13 +1,30 @@
 from urllib.request import urlopen
+import json
+import datetime
 
-# Link to website api
-link = 'https://wttr.in/Perth?A'
+# Init Variables
+welcomeMessage = "--- Weather Pyvien ---"
+date = datetime.datetime.now()
+date = date.strftime("%y/%-m/%-d")
 
-# Gets url and opens it
-wget = urlopen(link)
+# Welcome to the Weather API
+print(welcomeMessage)
 
-# Reads whats on website in clear text
-webtext = wget.read()
+# User search
+userSearch = input("Where would you like to check the weather?\n> ")
+
+# Get WOEID
+link = 'https://www.metaweather.com/api/location/search/?query=' + userSearch
+response = urlopen(link)
+data = json.loads(response.read())
+woeid = data[0]['woeid']
+
+# Get Weather
+link = 'https://www.metaweather.com/api/???/' + int(woeid) + '/' + date
+response = urlopen(link)
+data = json.loads(response.read()
 
 # Print it to the screen
-print(webtext.decode('utf-8'))
+print("Type of weather: " + data[0]["weather_state_name"])
+print("Minimum Temperature: " + str(round(data[0]["min_temp"])))
+print("Maximum Temperature: " + str(round(data[0]["max_temp"])))
